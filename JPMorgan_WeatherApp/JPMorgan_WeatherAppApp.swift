798fd5802f6edfc14aad70d0d2b89f324c187a72
Binh Huynh <bin.huy@gmail.com>
2023-02-26 10:38:38 -0800

    Initial Commit

---
+++ JPMorgan_WeatherApp/JPMorgan_WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPMorgan_WeatherAppApp.swift
+//  JPMorgan_WeatherApp
+//
+//  Created by Binh Huynh on 2/26/23.
+//
+
+import SwiftUI
+
+@main
+struct JPMorgan_WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
